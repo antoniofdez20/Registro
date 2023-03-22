85fe24c23f4e2473b6bfb08a021352c04d1d0be9
miquelmartorell2 <miquelmartorell2@gmail.com>
2023-03-22 20:03:18 +0100

    Añadir get y set en Registro usuario

--- RegistroUsuario.java
+++ RegistroUsuario.java
@@ -17,31 +17,65 @@ public class RegistroUsuario
         this.codigo = codigo;
         this.characters = characters;
 
-        Scanner sc = new Scanner(System.in);
-        Random random = new Random();
-        StringBuilder securityCode = new StringBuilder();
-        for (int i = 0; i < 8; i++) 
-        {
-            int index = random.nextInt(characters.length());
-            char character = characters.charAt(index);
-            securityCode.append(character);
-        }
-
-        System.out.println("Introduce tu nombre de usuario: ");
-        nombre = sc.nextLine();
-        System.out.println("Introduce correo de registro: ");
-        email = sc.nextLine();
-        System.out.println("Introduce la contraseña: ");
-        password = sc.nextLine();
-        System.out.println("Introduce el siguiente código de seguridad: " + securityCode.toString());
-        codigo = sc.nextLine();
-        sc.close();
+        StringBuilder securityCode = scanner();
 
         new ValidarCampos(nombre, email, password, codigo, securityCode);
     }
+
+    private StringBuilder scanner() {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        StringBuilder securityCode = new StringBuilder();
+        
+        System.out.println("Introduce tu nombre de usuario: ");
+        setNombre(sc.nextLine());
+        System.out.println("Introduce correo de registro: ");
+        setEmail(sc.nextLine());
+        System.out.println("Introduce la contraseña: ");
+        setPassword(sc.nextLine());
+        System.out.println("Introduce el siguiente código de seguridad: " + securityCode.toString());
+        setCodigo(sc.nextLine());
+        sc.close();
+        return securityCode;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getEmail(){
+       return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
 
     public static void main(String[] args) 
     {
         new RegistroUsuario();    
     }
+
+
 }
